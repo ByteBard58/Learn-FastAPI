c892fd5708ec57c4b1426c3d84ad80a7a042109b
Sakib Hossain <sakibhossainalif29@gmail.com>
2026-03-27 00:15:37 +0600

    Add query params with `/products` route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, HTTPException
 import numpy as np
 from product import process_data
 
@@ -15,3 +15,22 @@ def products(id):
     for dickt in whole_list:
         if id == dickt["id"]:
             return dickt
+
+@app.get("/products")
+def product_query(name:str = Query(default=None, 
+  min_length=1,
+  max_length=50,
+  description="Search By Product Name")) -> dict:
+
+    whole_list = process_data()
+    if name:
+        name_mod:str = name.strip().lower()
+        matches:list[dict] = [m for m in whole_list if name_mod in m["name"].lower()]
+        if not matches:
+            raise HTTPException(status_code=404,detail=f"No matches found for your search, '{name}'")
+    
+        return {
+            "total":len(matches), "items":matches
+        }
+    else: 
+        return {"total":len(whole_list),"items":whole_list}
