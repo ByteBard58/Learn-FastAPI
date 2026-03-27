582da52de4a3b8cd231775c2d25f1c0b2cf111f1
Sakib Hossain <sakibhossainalif29@gmail.com>
2026-03-27 20:39:10 +0600

    Modify `app.py` query logic

--- app.py
+++ app.py
@@ -32,40 +32,16 @@ def product_query(
   )
   ) -> dict:
 
-    whole_list = process_data()
-    to_return = []
-    if name:
+    results = process_data()
+    if name is not None:
         name_mod:str = name.strip().lower()
-        matches_1:list[dict] = [m for m in whole_list if name_mod in m["name"].lower()]
-        if not matches_1:
-            raise HTTPException(status_code=404,detail=f"No matches found for your search, '{name}'")
-        to_return.extend(matches_1)
+        results = [r for r in results if name_mod in r["name"].lower()]
+    if price_min is not None:
+        results = [r for r in results if r["price"] >= price_min]
+    if price_max is not None:
+        results = [r for r in results if r["price"] <= price_max]
+
+    if not results :
+        raise HTTPException(status_code=404, detail="No item found")
     
-    if price_min:
-        if to_return:
-            matches_2:list[dict] = [m for m in to_return if m["price"] >= price_min]
-            if not matches_2:
-                raise HTTPException(status_code=404, detail=f"No matches found for your search, 'Minimum price = {price_min}' and 'Name = {name}'")
-            to_return = [x for x in to_return if x in matches_2]
-        else:
-            matches_2:list[dict] = [m for m in whole_list if m["price"] >= price_min]
-            if not matches_2:
-                raise HTTPException(status_code=404,detail=f"No matches found for your search, 'Minimum price = {price_min}'")
-            to_return.extend(matches_2)
-
-    if price_max:
-        if to_return:
-            matches_3:list[dict] = [m for m in to_return if m["price"] <= price_max]
-            if not matches_3:
-                raise HTTPException(status_code=404, detail=f"No matches found for your search, 'Minimum price = {price_min}', 'Maximum price = {price_max}' and 'Name = {name}'")
-            to_return = [x for x in to_return if x in matches_3]
-        else:
-            matches_3:list[dict] = [m for m in whole_list if m["price"] <= price_max]
-            if not matches_3:
-                raise HTTPException(status_code=404,detail=f"No matches found for your search, 'Maximum price = {price_max}'")
-            to_return.extend(matches_3)
-
-    if to_return:
-        return {"total":len(to_return),"items":to_return}
-    else: 
-        return {"total":len(whole_list),"items":whole_list}
+    return {"total":len(results),"items":results}
