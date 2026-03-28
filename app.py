44a0927896d395aa3d039ee12ed58c2e50145c30
Sakib Hossain <sakibhossainalif29@gmail.com>
2026-03-28 16:16:27 +0600

    Add sorting functionality in `app.py`

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query, HTTPException
+from fastapi import FastAPI, Query, HTTPException,Path
 import numpy as np
 from product import process_data
 
@@ -24,11 +24,21 @@ def product_query(
        description="Search By Product Name"),
   price_min:float = Query(
       default=None, 
-      description="Provide the MINIMUM price of the product that you want to see"
+      description="Provide the MINIMUM price of the product that you want to see",
   ),
   price_max:float = Query(
       default=None,
       description="Provide the MAXIMUM price of the product that you want to see"
+  ),
+  sort:bool = Query(
+      default=True,
+      description="Sort the items according to price"
+  ),
+  order:int = Query(
+      default=0,
+      ge=0,
+      le=1,
+      description="0 for ascending, 1 for descending"
   )
   ) -> dict:
 
@@ -40,8 +50,14 @@ def product_query(
         results = [r for r in results if r["price"] >= price_min]
     if price_max is not None:
         results = [r for r in results if r["price"] <= price_max]
-
     if not results :
         raise HTTPException(status_code=404, detail="No item found")
+    if sort:
+        if order not in [0,1]:
+            raise HTTPException(status_code=422,detail=f"Only 0(ascending) and 1(descending) are accepted as order, got {order}")
+        else:
+            reverse = True if order == 1 else False
+            results = sorted(results,key=lambda x: x.get("price"),reverse=reverse)
+            return {"total":len(results),"items":results}
     
     return {"total":len(results),"items":results}
