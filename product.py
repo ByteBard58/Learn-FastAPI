82204bcfc0e9235631e20543447f02a90eebb032
Sakib Hossain <sakibhossainalif29@gmail.com>
2026-03-29 08:41:30 +0600

    Add if-else structure in `product.py`

--- product.py
+++ product.py
@@ -7,8 +7,9 @@ TARGET = Path("Data","products.json")
 def process_data(target = TARGET) -> list[dict]:
   if not os.path.exists(TARGET):
     return []
-  with open(TARGET,"r",encoding="utf-8") as file:
-    return json.load(file)
+  else:
+    with open(TARGET,"r",encoding="utf-8") as file:
+      return json.load(file)
 
 def main() -> None:
   process_data()
